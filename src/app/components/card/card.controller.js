2d9133fa3f41a61e76f3fc330af979bb9958b964
Daniel King <daniel.king5@nhs.net>
2017-06-13 18:21:12 +0100

    WIP add edit action button to cards

--- src/app/components/card/card.controller.js
+++ src/app/components/card/card.controller.js
@@ -2,42 +2,58 @@
   'use strict';
 
   angular
-  .module('asylumjourneyFrontend')
-  .controller('CardController', CardController);
+    .module('asylumjourneyFrontend')
+    .controller('CardController', CardController);
 
-    function CardController($scope, ngDialog, $routeParams, $rootScope) {
-        var vm = this;
-        vm.service = $scope.service;
-        angular.forEach($scope.service._embedded.providers, function(provider) {
-          var googleMapsUrl = 'https://www.google.co.uk/maps/place/';
-          provider.addressUrl = googleMapsUrl + [provider.address, provider.postcode]
-            .join(' ')
-            .replace(new RegExp(/\s/g, 'g'), '+');
-        });
+  function CardController(data, $scope, ngDialog, $routeParams, $rootScope, $log) {
+    var vm = this;
+    vm.service = $scope.service;
+    vm.openDialog = openDialog;
+    vm.editService = editService;
 
+    angular.forEach($scope.service._embedded.providers, function(provider) {
+      var googleMapsUrl = 'https://www.google.co.uk/maps/place/';
+      provider.addressUrl = googleMapsUrl + [provider.address, provider.postcode]
+        .join(' ')
+        .replace(new RegExp(/\s/g, 'g'), '+');
+    });
 
-		vm.openDialog = openDialog;
+    activate();
 
-        activate();
-
-        function activate() {
-			checkRoute();
-        }
-
-		function checkRoute() {
-			// permalink to modal
-			if (!$rootScope.dialogOpen && (parseInt($routeParams.serviceId) === vm.service.id)) {
-				vm.openDialog();
-				$rootScope.dialogOpen = true;
-			}
-		}
-
-        function openDialog() {
-            ngDialog.open({
-                template: 'app/components/detail-overlay/detail.html',
-                scope: $scope
-            });
-        }
+    function activate() {
+      checkRoute();
     }
+
+    function checkRoute() {
+      // permalink to modal
+      if (!$rootScope.dialogOpen && (parseInt($routeParams.serviceId) === vm.service.id)) {
+        vm.openDialog();
+        $rootScope.dialogOpen = true;
+      }
+    }
+
+    function getProviders() {
+      return data.providers().get().$promise.then(function(response) {
+        $scope.providers = response._embedded.providers;
+      });
+    }
+
+    function openDialog() {
+      ngDialog.open({
+        template: 'app/components/detail-overlay/detail.html',
+        scope: $scope
+      });
+    }
+
+    function editService() {
+      getProviders().then(function() {
+        ngDialog.close();
+        ngDialog.open({
+          template: 'app/components/detail-overlay/edit.html',
+          scope: $scope
+        });
+      });
+    }
+  }
 
 })();
